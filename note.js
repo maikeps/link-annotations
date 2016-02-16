b584074f3324f6b1362b756edd15c14558b906ac
=lucasmpalma <=lucaspalma.m@gmail.com>
2016-02-16 15:04:58 -0200

    anotações salvas para cada link

--- note.js
+++ note.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 	img_url = chrome.extension.getURL("img/note.png");
 	
-	$("h3.r").prepend("<img id='note_icon' src=" + img_url + " width=16 height=16>");
-	$("#note_icon").click(function(){
-		note = prompt("Annotation:");
-		alert("Your annotation:\n" + note);
+	$("h3.r").prepend("<img class='note_icon' src=" + img_url + " width=16 height=16> &nbsp");
+	
+	$("img.note_icon").click(function(){
+		link = $(this).next()[0].href
+	
+		chrome.storage.sync.get(link, function(obj) {
+	 		if (jQuery.isEmptyObject(obj)){
+				note = prompt("Annotation:");
+				dict = {}
+				dict[link] = note;
+				chrome.storage.sync.set(dict)
+	 		} else {
+	 			alert("Your Annotation:\n" + obj[link])		
+	 		}
+ 		});
 	});
 });
